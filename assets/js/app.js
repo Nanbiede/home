2bb023f0408d276e31050571e15499001897a215
Didiertose <didiertose@gmail.com>
2022-04-19 17:32:45 +0100

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,28 @@
+// vérification de la présence des services Workers dans le navigateur
 if("serviceWorker" in navigator)
 {
-    navigator.serviceWorker.register('/home/serviceWorker.js');
+    navigator.serviceWorker.register('/serviceWorker.js');
 }
+//fin de la vérification
+
+
+//alert au micro
+var microPhone = document.getElementById('micro');
+function alertMicro() {
+    alert("La fonctionalité micro n'est pas encore disponible, ...")
+}
+microPhone.addEventListener("click", alertMicro);
+
+//fin alert au micro
+
+// selection des produits sur la page index.html
+var produits = document.getElementsByClassName(".produit");
+
+
+//récupération de l'année en cours
+var annee = new Date();
+var anneeEnCours = document.getElementById("annee_encours");
+anneeEnCours.innerHTML = annee.getFullYear();
+anneeEnCours.style.FontFamily = "Orelega One";
+
+// gestion de la requete ajax pour rechercher un produit
