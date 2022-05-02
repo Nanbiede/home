ab57cb152850fcfbc50ef33b9440c99712fed627
Didiertose <didiertose@gmail.com>
2022-05-02 10:51:31 +0200

    lundi0205

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-## Welcome to Nanbiédé
+# Bienvenu sur Nanbiédé
+    
+Nanbiédé est un projet de developpement de Mlle **Brunice PADONOU**.
+
+    Le projet vise à offrir une plateforme à la populilation béninoise sur le prix des denrées sur le marché au pays.
+
+## Pourquoi **Nanbiédé** ?
+
+## Toutes la puissance de Nanbiédé.
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,51 @@
-// vérification de la présence des services Workers dans le navigateur
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
 if("serviceWorker" in navigator)
 {
-    navigator.serviceWorker.register('/home/serviceWorker.js');
+    navigator.serviceWorker.register('/serviceWorker.js');
 }
-//fin de la vérification
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
 
 
-//alert au micro
+/////////////////////////////////////////////////////////////////////
+//alert au micro/////////////////////////////////////////////////////
 var microPhone = document.getElementById('micro');
 function alertMicro() {
     alert("La fonctionalité micro n'est pas encore disponible, ...")
 }
 microPhone.addEventListener("click", alertMicro);
 
-//fin alert au micro
+/////////////////////////////////////////////////////////////////////
+// selection des produits sur la page index.html/////////////////////
+var produits = document.getElementsByClassName('produit');
 
-// selection des produits sur la page index.html
-var produits = document.getElementsByClassName(".produit");
+var modalBackEnd = document.getElementsByClassName('global_details_produit')[0];
+var modalContent = document.getElementsByClassName('details_produit')[0];
+var closeBtn = document.getElementsByClassName("close_btn")[0];
+
+for (let index = 0; index < produits.length; index++) {
+    var element = produits[index];
+    element.addEventListener("click", ()=>{
+        window.location.href="#reference_haut";
+        modalBackEnd.style.display="block";
+        closeBtn.addEventListener("click", ()=>{
+            modalBackEnd.style.display="none";
+        })
+    });
+}
 
 
-//récupération de l'année en cours
+
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////////////////////////////////
+//récupération de l'année en cours///////////////////////////////////
 var annee = new Date();
 var anneeEnCours = document.getElementById("annee_encours");
 anneeEnCours.innerHTML = annee.getFullYear();
 anneeEnCours.style.FontFamily = "Orelega One";
-
-// gestion de la requete ajax pour rechercher un produit
+// gestion de la requete ajax pour rechercher un produit/////////////
+/////////////////////////////////////////////////////////////////////
