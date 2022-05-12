a635968a2f13ec0c17a3f66aa356578bd1d061ea
Didiertose <didiertose@gmail.com>
2022-05-12 10:18:51 +0200

    v112052022

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,51 +1,40 @@
-/////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////
-if("serviceWorker" in navigator)
-{
-    navigator.serviceWorker.register('/serviceWorker.js');
-}
-/////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////
+//on vérifie si le navigateur gère les serviceWorkers
+    if("serviceWorker" in navigator)
+    {
+        navigator.serviceWorker.register('/serviceWorker.js');
+    }
+//fin des serviceWorkers
 
 
-/////////////////////////////////////////////////////////////////////
-//alert au micro/////////////////////////////////////////////////////
-var microPhone = document.getElementById('micro');
-function alertMicro() {
-    alert("La fonctionalité micro n'est pas encore disponible, ...")
-}
-microPhone.addEventListener("click", alertMicro);
+//alert au micro
+    var microPhone = document.getElementById('micro');
+    function alertMicro() {
+        alert("La fonctionalité micro n'est pas encore disponible, ...")
+    }
+    microPhone.addEventListener("click", alertMicro);
+//fin alert au micro
 
-/////////////////////////////////////////////////////////////////////
-// selection des produits sur la page index.html/////////////////////
-var produits = document.getElementsByClassName('produit');
-
-var modalBackEnd = document.getElementsByClassName('global_details_produit')[0];
-var modalContent = document.getElementsByClassName('details_produit')[0];
-var closeBtn = document.getElementsByClassName("close_btn")[0];
-
-for (let index = 0; index < produits.length; index++) {
-    var element = produits[index];
-    element.addEventListener("click", ()=>{
-        window.location.href="#reference_haut";
-        modalBackEnd.style.display="block";
-        closeBtn.addEventListener("click", ()=>{
-            modalBackEnd.style.display="none";
-        })
-    });
-}
+// selection des produits sur la page index.html
+    var produits = document.getElementsByClassName('produit');
+    var modalBackEnd = document.getElementsByClassName('global_details_produit')[0];
+    var modalContent = document.getElementsByClassName('details_produit')[0];
+    var closeBtn = document.getElementsByClassName("close_btn")[0];
+    //boucle de répétition sur chaque produit
+        for (let index = 0; index < produits.length; index++) {
+            var element = produits[index];
+            element.addEventListener("click", ()=>{
+                window.location.href="#reference_haut";
+                modalBackEnd.style.display="block";
+                closeBtn.addEventListener("click", ()=>{
+                    modalBackEnd.style.display="none";
+                })
+            });
+        }
 
 
+//récupération de l'année en cours
+    var annee = new Date();
+    var anneeEnCours = document.getElementById("annee_encours");
+    anneeEnCours.innerHTML = annee.getFullYear();
 
-/////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////
-
-
-/////////////////////////////////////////////////////////////////////
-//récupération de l'année en cours///////////////////////////////////
-var annee = new Date();
-var anneeEnCours = document.getElementById("annee_encours");
-anneeEnCours.innerHTML = annee.getFullYear();
-anneeEnCours.style.FontFamily = "Orelega One";
-// gestion de la requete ajax pour rechercher un produit/////////////
-/////////////////////////////////////////////////////////////////////
+// gestion de la requete ajax pour rechercher un produit
